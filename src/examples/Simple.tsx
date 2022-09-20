26e4fc95d337e120898350f5ee794b47f79fabf1
rodogir <me@rodogir.com>
2022-09-20 21:52:11 +0200

    add title to form

--- src/examples/Simple.tsx
+++ src/examples/Simple.tsx
@@ -1,7 +1,7 @@
+import { FireIcon } from "@heroicons/react/24/outline";
 import { Form, useForm } from "../form/useForm";
 import { ResetButton, SubmitButton, TextInputField } from "./components";
 import { FormStateMessage, waitFor } from "./helpers";
-import { FireIcon } from "@heroicons/react/24/outline";
 
 export function Simple() {
   const form = useForm({
@@ -22,25 +22,30 @@ export function Simple() {
   };
 
   return (
-    <Form
-      id="sample-1"
-      formInstance={form}
-      onSubmit={simulateSubmit}
-      className="flex flex-col gap-3 max-w-4xl"
-    >
-      <div className="grid grid-cols-3 gap-2">
-        <TextInputField name="firstName" label="Given name" />
-        <TextInputField name="lastName" label="Last name" />
-        <TextInputField name="fullName" label="Full name" />
-      </div>
-      <div className="flex gap-2 justify-end">
-        <ResetButton onClick={form.reset} />
-        <SubmitButton name="submit" value="error">
-          <FireIcon className="h-5 w-5 text-rose-700" />
-        </SubmitButton>
-        <SubmitButton name="submit" value="success" />
-      </div>
-      <FormStateMessage />
-    </Form>
+    <section>
+      <h1 className="text-2xl font-semibold text-gray-800 capitalize">
+        Simple Example
+      </h1>
+      <Form
+        id="sample-1"
+        formInstance={form}
+        onSubmit={simulateSubmit}
+        className="mt-6 flex flex-col gap-3 max-w-4xl"
+      >
+        <div className="grid grid-cols-3 gap-2">
+          <TextInputField name="firstName" label="Given name" />
+          <TextInputField name="lastName" label="Last name" />
+          <TextInputField name="fullName" label="Full name" />
+        </div>
+        <div className="flex gap-2 justify-end">
+          <ResetButton onClick={form.reset} />
+          <SubmitButton name="submit" value="error">
+            <FireIcon className="h-5 w-5 text-rose-700" />
+          </SubmitButton>
+          <SubmitButton name="submit" value="success" />
+        </div>
+        <FormStateMessage />
+      </Form>
+    </section>
   );
 }
