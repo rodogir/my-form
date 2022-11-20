ed48f102ee5a67617102d7acd3a00b88ea876212
rodogir <me@rodogir.com>
2022-11-20 14:29:30 +0100

    fixate port

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+	server: {
+		port: 3700,
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {
