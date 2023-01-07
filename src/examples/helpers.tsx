5c90851e2e1cf831d158ae77a10e5b8726820a35
rodogir <me@rodogir.com>
2023-01-07 12:32:58 +0100

    improve form state feedback

--- src/examples/helpers.tsx
+++ src/examples/helpers.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+import { FormMetaState } from "../../lib/form-state";
 import { useFormMeta } from "../../lib/main";
 
 export function waitFor(ms: number) {
@@ -8,26 +10,33 @@ export function waitFor(ms: number) {
 
 export function FormStateMessage({ values }: { values: unknown }) {
 	const { state, submitCount } = useFormMeta();
+	const hasValues = Boolean(values) && state === "submitted";
 	return (
-		<>
-			{state === "submitting" && <p>Submitting ...</p>}
-			{state === "submitted" && (
-				<>
-					<p>✅ Form successfully submitted. submit count: {submitCount}</p>
-					<details>
-						<summary data-testid="toggle-submitted-values">
-							Submitted values
-						</summary>
-						<pre
-							data-testid="submitted-values"
-							data-values={JSON.stringify(values)}
-						>
-							{JSON.stringify(values, undefined, 2)}
-						</pre>
-					</details>
-				</>
-			)}
-			{state === "error" && <p>❌ Oh no, submitting the form failed 😔</p>}
-		</>
+		<aside>
+			<h3 data-testid="form-state" data-form-state={state}>
+				{messages[state]}
+			</h3>
+			<p>Submit count: {submitCount}</p>
+			{hasValues && <SubmittedValues values={values} />}
+		</aside>
+	);
+}
+
+const messages: Record<FormMetaState, ReactNode> = {
+	valid: "Form is valid",
+	validating: "Validating form ...",
+	error: "❌ Oh no, submitting the form failed 😔",
+	submitted: "✅ Form successfully submitted! 🎉",
+	submitting: "Submitting ...",
+};
+
+function SubmittedValues({ values }: { values: unknown }) {
+	return (
+		<details>
+			<summary data-testid="toggle-submitted-values">Submitted values</summary>
+			<pre data-testid="submitted-values" data-values={JSON.stringify(values)}>
+				{JSON.stringify(values, undefined, 2)}
+			</pre>
+		</details>
 	);
 }
